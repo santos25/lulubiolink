1e1db41130f378810fc705c303c919b729d3cce6
Andres Santos <asantos@dcatech.com>
2020-12-01 16:39:31 -0500

    New Link Catalogos

--- public/index.js
+++ public/index.js
@@ -1,30 +1,31 @@
-const btnCatalogo = document.querySelector("#btnCatalogo")
-const btnMayor = document.querySelector("#btnMayor")
-const btnDetail = document.querySelector("#btnDetail")
+const btnCatalogo = document.querySelector("#btnCatalogo");
+// const btnMayor = document.querySelector("#btnMayor")
+const btnDetail = document.querySelector("#btnDetail");
 
-const numero = '573163934994'
+const numero = "573163934994";
 function openCatalogo(e) {
-    window.open("https://drive.google.com/drive/folders/1qDvh5Q1MDXbZqLfG66xHHzs3BxP-Q5SC?usp=sharing", "_blank")
+  window.open(
+    "https://drive.google.com/drive/folders/1mbullnewRXp5BnHf8FCAgIXSiJ--wqmu?usp=sharing",
+    "_blank"
+  );
 }
 
 function messageMayorWhatsApp(e) {
-    console.log("Sending Message");
-    const message = "Hola, quiero una asesoría mayorista";
-    const url = 'https://wa.me/' + numero + "/?text=" + encodeURI(message);
-    console.log(encodeURIComponent(url));
-    window.open(url, "_blank")
-
+  // console.log("Sending Message");
+  const message = "Hola, quiero una asesoría mayorista";
+  const url = "https://wa.me/" + numero + "/?text=" + encodeURI(message);
+  console.log(encodeURIComponent(url));
+  window.open(url, "_blank");
 }
 
 function messageDetailWhatsApp(e) {
-    console.log("Sending Message");
-    const message = "Hola, deseo comprar al detal.";
-    const url = 'https://wa.me/' + numero + "/?text=" + encodeURI(message);
-    console.log(encodeURIComponent(url));
-    window.open(url, "_blank")
-
+  // console.log("Sending Message");
+  const message = "Hola, requiero información sobre los productos .";
+  const url = "https://wa.me/" + numero + "/?text=" + encodeURI(message);
+  // console.log(encodeURIComponent(url));
+  window.open(url, "_blank");
 }
 
-btnCatalogo.addEventListener("click", openCatalogo)
-btnMayor.addEventListener("click", messageMayorWhatsApp)
-btnDetail.addEventListener("click", messageDetailWhatsApp)
+btnCatalogo.addEventListener("click", openCatalogo);
+// btnMayor.addEventListener("click", messageMayorWhatsApp)
+btnDetail.addEventListener("click", messageDetailWhatsApp);
